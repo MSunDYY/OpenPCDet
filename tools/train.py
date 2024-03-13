b89951586686ab0cb6af8d7be2b973404a844ffd
MSunDYY <2563733583@qq.com>
2024-03-13 16:55:19 +0800

    fasdf

--- tools/train.py
+++ tools/train.py
@@ -124,7 +124,12 @@ def main(args, cfgs):
     if cfg.LOCAL_RANK == 0:
         os.system('cp %s %s' % (args.cfg_file, output_dir))
 
-    tb_log = SummaryWriter(log_dir=str(output_dir / 'tensorboard'),filename_suffix='latest_event.out.tfevents') if cfg.LOCAL_RANK == 0 else None
+    if cfg.LOCAL_RANK == 0:
+
+        for file_name in os.listdir(output_dir/'tensorboard'):
+            os.remove(output_dir/'tensorboard'/file_name)
+
+    tb_log = SummaryWriter(log_dir=str(output_dir / 'tensorboard')) if cfg.LOCAL_RANK == 0 else None
 
     logger.info("----------- Create dataloader & network & optimizer -----------")
     train_set, train_loader, train_sampler = build_dataloader(
