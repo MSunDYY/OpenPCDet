e939aa79f1514ad6a32b2374faa991d5827487cc
MSunDYY <2563733583@qq.com>
2024-09-11 14:42:56 +0800

    drop

--- pcdet/models/roi_heads/denet_head5.py
+++ pcdet/models/roi_heads/denet_head5.py
@@ -302,9 +302,11 @@ class KPTransformer(nn.Module):
                                      nn.Linear(self.channels,7))
 
         self.conv1 = nn.Sequential(
-            nn.Conv1d(self.channels*4,self.channels,1,1),
-            nn.BatchNorm1d(self.channels),
+            nn.Conv1d(self.channels*4,self.channels*2,1,1),
+            nn.BatchNorm1d(self.channels*2),
             nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Conv1d(self.channels*2,self.channels,1)
         )
         self.linear1 = nn.ModuleList([nn.Linear(self.channels*2,self.channels) for _ in range(self.num_groups)])
         self.dropout1 = nn.ModuleList([nn.Dropout(0.1) for _ in range(self.num_groups)])
@@ -313,10 +315,11 @@ class KPTransformer(nn.Module):
         self.norm1 = nn.LayerNorm(self.channels)
 
         self.conv2 = nn.Sequential(
-            nn.Conv1d(self.channels * 4, self.channels , 1, 1),
-            nn.BatchNorm1d(self.channels ),
+            nn.Conv1d(self.channels * 4, self.channels*2 , 1, 1),
+            nn.BatchNorm1d(self.channels*2 ),
             nn.ReLU(),
-
+            nn.Dropout(0.1),
+            nn.Conv1d(self.channels*2,self.channels,1)
         )
         self.linear2 = nn.ModuleList([nn.Linear(self.channels*2 , self.channels) for _ in range(self.num_groups)])
         self.norm2 = nn.LayerNorm(self.channels)
